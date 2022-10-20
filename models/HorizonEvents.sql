159820379960ad58133c35237f23cbcbdf260677
Ana Carolina <102334478+AnaCarolinaMello@users.noreply.github.com>
2022-10-20 19:44:45 -0300

    Update HorizonEvents.sql

--- models/HorizonEvents.sql
+++ models/HorizonEvents.sql
@@ -1,3 +1,4 @@
+
 CREATE DATABASE HorizonEvents;
 USE HorizonEvents;
 
@@ -12,32 +13,34 @@ Senha varchar(90) not null,
 Primary key(Id)
 )Engine=INNODB;
 
-CREATE TABLE Usuario_Empresa(
-CNPJ char(20) not null,
-Razao_Social varchar(100) not null,
+CREATE TABLE Empresa(
+Id int unsigned auto_increment not null,
+CNPJ char(18) not null,
+Razao_Comercial varchar(100) not null,
 Nome_Fantasia varchar(90) not null,
 Descricao varchar(255),
 Website varchar(100),
-Foto_Perfil blob,
-Imagem_Demostrativa1 blob,
-Imagem_Demostrativa2 blob,
-Imagem_Demostrativa3 blob,
-Email varchar(80) not null,
-Telefone char(15) not null,
+Foto_Perfil varchar(80),
+Imagem_Demostrativa1 varchar(80),
+Imagem_Demostrativa2 varchar(80),
+Imagem_Demostrativa3 varchar(80),
+Representante varchar(80) not null,
+Telefone char(15),
+Numero varchar(80) not null,
 Rua varchar(80) not null,
-Bairro varchar(80) not null,
-Cidade varchar(80) not null,
-UF char(2) not null,
-CEP char(10) not null,
+Bairro varchar(80),
+Cidade varchar(80),
+UF char(2),
+CEP char(9) not null,
 Senha varchar(100) not null,
-primary key(CNPJ)
+primary key(Id)
 )Engine=INNODB;
 
 CREATE TABLE Favoritos(
 Id int unsigned auto_increment not null,
 Usuario_Id int unsigned not null,
-Empresa_CNPJ char(20) not null,
+Empresa_Id int unsigned not null,
 primary key(id),
 foreign key(Usuario_Id) references Usuario_Cliente(Id),
-foreign key(Empresa_CNPJ) references Usuario_Empresa(CNPJ)
+foreign key(Empresa_Id) references Empresa(Id)
 )Engine=INNODB;
