0e73d48ab50e9d9fb94ff430756edde712105835
Ana Carolina <102334478+AnaCarolinaMello@users.noreply.github.com>
2022-10-31 17:13:15 -0300

    Update route.js

--- routes/route.js
+++ routes/route.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const route = express.Router()
 
@@ -33,6 +32,14 @@ route.get("/business/Signup", (req,res)=>{
         title: "Entrar",
         style: "cadastroEmpresa.css",
         script: "businessSignup.js"
+    })
+})
+
+route.get('/business/Login',(req,res)=>{
+    res.render("business/businessLogin",{
+        title: "Login",
+        style: "businessLogin.css",
+        script: "businessLogin.js"
     })
 })
 
